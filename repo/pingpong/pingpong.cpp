4993c051ec441f922cf6d5c0ebe2f18be6ccded8
stephanielam3211 <lam44@dane1.llnl.gov>
2026-09-25 16:06:58 -0700

    fixing timing

--- repo/pingpong/pingpong.cpp
+++ repo/pingpong/pingpong.cpp
@@ -1122,7 +1122,7 @@ int main(int argc, char **argv)
                 double iter_max = 0.0;
                 MPI_Reduce(&dt, &iter_max, 1, MPI_DOUBLE, MPI_MAX, 0, region_comm);
 
-                if(rank == 0)
+                if(region_rank == 0)
                 {
                         // red_total_time += iter_max;
                         // if(iter_max < min_rtt) min_rtt = iter_max;      //fix min and max calculation
